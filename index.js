e7592a41c57e89b355566e0afc8f04535cc1c101
habibur rahman <habiburrahmanofficial157@gmail.com>
2023-11-16 02:44:36 +0600

    create  a admin route and user sent to admin and admin must be deleted user

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+var jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -31,6 +32,10 @@ async function run() {
     const cartsCollection = client.db("bistroDB").collection("carts");
 
     //  user related api
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
 
     app.post("/users", async (req, res) => {
       const user = req.body;
@@ -42,6 +47,23 @@ async function run() {
         const result = await userCollection.insertOne(user);
         res.send(result);
       }
+    });
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
     });
 
     // menu related collections
